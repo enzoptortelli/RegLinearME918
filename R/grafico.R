273a1784c2e50f72a9fb5206714b03c6d812df46
Lara Maria Herrera Drugowick <80405811+laradru@users.noreply.github.com>
2024-09-26 15:50:49 -0300

    Alteração da função gráfico

--- R/grafico.R
+++ R/grafico.R
@@ -1,17 +1,31 @@
-grafico <- function(modelo, tipo) {
-  # verificar classe do modelo
+# Gráfico de Valores Preditos vs Observados (pvo)
+if (tipo == "pvo") {
+  result <- plot(modelo$dados[, 1], modelo$ajustados,
+                 xlab = "Valores Observados",
+                 ylab = "Valores Preditos",
+                 main = "Valores Preditos vs Observados",
+                 pch = 19, col = "blue")
+  #abline(a = 0, b = 1, col = "red", lwd = 2) 
+}
+# Gráfico de Resíduos vs Ordem de coleta (rvo)
+else if (tipo == "rvo") {
+  result <- plot(modelo$dados[,2], modelo$residuos,
+                 xlab = "Ordem",
+                 ylab = "Resíduos",
+                 main = "Resíduos vs Ordem",
+                 pch = 19, col = "blue")
+  #abline(h = 0, col = "red", lwd = 2)
+}
+# Qualquer outro tipo
+else {
+  stop("Erro: tipo de gráfico inválido. Use 'pvo' para preditos vs observados ou 'rvo' para resíduos vs ordem de coleta.")
+}
 
-  # predito vs observado (pvo)
-  # residuos vs ordem (rvo)
-  # ...
-  if(tipo == "pvo") {
-    result <- plot(modelo$dados[, 1], modelo$ajustados)
-  }
-  else if(tipo == "rvo") {
-    result <- plot(modelo$residuos)
-  }
-
-  return(result)
+return(result)
 }
 
 #Coloquei as funções que estavam em regressão.linear.R em arquivos separados, e comecei a fazer a documentação da função reg_linear.
+
+summary(modelolara)
+grafico(modelolara, tipo = "pvo")
+
