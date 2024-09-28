dcd71fa0d0f7892f292f99f77da4ba067cb3ad15
laradru <laraherrera0@gmail.com>
2024-09-27 23:27:57 -0300

    Update grafico.R
    Correção da função "grafico" para  regressão múltipla e início da documentação da função.

--- R/grafico.R
+++ R/grafico.R
@@ -1,36 +1,65 @@
+#' @title Gráficos de resíduos
+#' @description
+#' \code{grafico} Função utilizada para criação de gráficos de resíduos em regressão linear. Válida também para regressão linear múltipla.
+#'
+#'
+#' @param modelo Modelo de regressão linear simples ou múltipla. Deve ser, obrigatoriamente, da classe \code{"modelo_linear"}.
+#' Deve conter os elementos \code{dados}, \code{residuos} (ou seja, os resíduos do modelo) e \code{ajustados} (ou seja, os valores ajustados (preditos) pelo modelo). 
+#' O modelo fornecido deve ter a classe "modelo_linear", caso contrário, a função gerará um erro. 
+#' @param tipo É uma string que especifica o tipo de gráfico desejado. Os valores aceitos são:
+#' \describe{
+#'   \item{rvp}{Gera gráficos de Resíduos vs Preditoras.}
+#'   \item{rva}{Gera um gráfico de Resíduos vs Valores Ajustados.}
+#'   \item{normres}{Gera um Gráfico QQPlot para avaliar a normalidade dos resíduos.}
+#' }
+#' 
+#'
+#' @examples
+#' grafico(modelo, "rvp")      # Gráfico de Resíduos vs Preditoras
+#' grafico(modelo, "rva")      # Gráfico de Resíduos vs Valores Ajustados
+#' grafico(modelo, "normres")  # Gráfico Q-Q de Normalidade dos Resíduos
+#'
+#'
+#' @export
 grafico <- function(modelo, tipo) {
   # verificar se a classe do modelo inserido pelo usuário é "modelo_linear":
   if (!"modelo_linear" %in% class(modelo)) {
     stop("Erro: o objeto fornecido não é um modelo linear válido.")
   }
-  # Gráfico de Valores Preditos vs Observados (pvo)
-  if (tipo == "pvo") {
-    result <- plot(modelo$dados[, 1], modelo$ajustados,
-                   xlab = "Valores Observados",
-                   ylab = "Valores Preditos",
-                   main = "Valores Preditos vs Observados",
-                   pch = 19, col = "blue")
-    #abline(a = 0, b = 1, col = "red", lwd = 2) 
+  # Gráfico de Resíduos vs Preditoras (rvp)
+  if (tipo == "rvp") {
+    colunas_preditoras <- colnames(modelo$dados[-1])
+    for (coluna_preditora in colunas_preditoras) {
+      plot(modelo$dados[,coluna_preditora], modelo$residuos,
+                     xlab = coluna_preditora,
+                     ylab = "Resíduos",
+                     main = "Resíduos vs Preditoras",
+                     pch = 19, col = "black")
+      abline(h = 0, col = "red", lwd = 2, lty = 5)
+    }
   }
-  # Gráfico de Resíduos vs Ordem de coleta (rvo)
-  else if (tipo == "rvo") {
-    result <- plot(modelo$dados[,2], modelo$residuos,
-                   xlab = "Ordem",
+  # Gráfico de Resíduos vs Valores Ajustados (rva)
+  else if (tipo == "rva") {
+    plot(modelo$ajustados, modelo$residuos,
+                   xlab = "Valores Preditos",
                    ylab = "Resíduos",
-                   main = "Resíduos vs Ordem",
-                   pch = 19, col = "blue")
-    #abline(h = 0, col = "red", lwd = 2)
+                   main = "Resíduos vs Preditos",
+                   pch = 19, col = "black")
+    abline(h = 0, col = "red", lwd = 2, lty = 5) 
+  }
+  # Gráfico de Normalidade dos Resíduos (normres)
+  else if (tipo == "normres") {
+      qqnorm(modelo$residuos) 
+      qqline(modelo$residuos, col = "red")
+      xlab = "Quantis Teóricos"
+      ylab = "Quantis Amostrais"
   }
   # Qualquer outro tipo
   else {
     stop("Erro: tipo de gráfico inválido. Use 'pvo' para preditos vs observados ou 'rvo' para resíduos vs ordem de coleta.")
   }
   
-  return(result)
 }
 
-#Coloquei as funções que estavam em regressão.linear.R em arquivos separados, e comecei a fazer a documentação da função reg_linear.
 
-summary(modelolara)
-grafico(modelolara, tipo = "pvo")
 
