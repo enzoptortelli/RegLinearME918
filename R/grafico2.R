aba42b9a242894134bc3a065b937605a8a4a3732
enzo <enzoptortelli@gmail.com>
2024-09-30 13:10:56 -0300

    Resolvi o conflito que deu entre minhas modificações e a do main

--- R/grafico2.R
+++ R/grafico2.R
@@ -7,21 +7,27 @@ grafico <- function(modelo, tipo) {
   result <- list()
   pvo_list <- list()
   rvp_list <- list()
+  qq_list <- list()
 
   var_respostas <- colnames(modelo$coeficientes)
   var_preditoras <- rownames(modelo$coeficientes)[-1]
 
-  salvar_plot <- function(dados, nome_var) {
-    ggplot(dados, aes())
-  }
 
+  getPlot <- function(dados, x, y) {
+    return(ggplot(dados, aes(x = .data[[x]], y = .data[[y]])) +
+             theme_classic())
+  }
 
   # Gráfico de Valores Preditos vs Observados (pvo)
   if("pvo" %in% tipo) {
     for(var_resposta in var_respostas) {
-      temp_dado <- as.data.frame(cbind(modelo$ajustados[, var_resposta], modelo$dados[, var_resposta]))
-      grafico <- ggplot(data = temp_dado,
-                        aes(x = temp_dado[, 2], y = temp_dado[, 1])) +
+      temp_dado <- data.frame(modelo$ajustados[, var_resposta], modelo$dados[, var_resposta])
+      temp_colnames <-c(paste(var_resposta, "ajustado", sep = '_'),
+                        paste(var_resposta, "observado", sep = '_'))
+
+      colnames(temp_dado) <- temp_colnames
+
+      grafico <- getPlot(temp_dado, x = temp_colnames[2], y = temp_colnames[1]) +
         geom_point() +
         labs(
           title = paste('Valores preditos x observados (', var_resposta, ')'),
@@ -36,35 +42,65 @@ grafico <- function(modelo, tipo) {
 
   # Gráfico de Resíduos vs Preditoras (rvp)
   if('rvp' %in% tipo) {
+    temp_nomes_list <- c()
     for(var_preditora in var_preditoras) {
-      temp_dado <- as.data.frame(cbind(modelo$residuos, modelo$dados[, var_preditora]))
-      grafico <- ggplot(data = temp_dado,
-                        aes(x = temp_dado[, 2], y = temp_dado[, 1])) +
-        geom_point() +
-        labs(
-          title = paste('Resíduos x preditora (', var_preditora, ')'),
-          x = 'Resíduos',
-          y = 'Preditora'
-        )
+      for(var_resposta in var_respostas) {
+        temp_dado <- data.frame(modelo$residuos[, var_resposta], modelo$dados[, var_preditora])
+        temp_colnames <-c('residuos',
+                          paste(var_resposta, "observado", sep = '_'))
+        colnames(temp_dado) <- temp_colnames
 
-      rvp_list <- append(rvp_list, list(grafico))
+        grafico <- getPlot(temp_dado, y = temp_colnames[1], x = temp_colnames[2]) +
+          geom_point() +
+          geom_hline(yintercept = 0, color = 'red') +
+          labs(
+            title = paste('Resíduos (', var_resposta, ') x  preditora (', var_preditora, ')'),
+            y = 'Resíduos',
+            x = 'Preditora'
+          )
+        rvp_list <- append(rvp_list, list(grafico))
+        temp_nomes_list <- c(paste(var_resposta, var_preditora, sep = '_'), temp_nomes_list)
 
+      }
     }
-    names(rvp_list) <- var_preditoras
+    names(rvp_list) <- temp_nomes_list
+  }
+
+  # QQ-plot dos resíduos
+  if('qqplot' %in% tipo) {
+    for(var_resposta in var_respostas) {
+      temp_dado <- data.frame(modelo$residuos[, var_resposta])
+      colnames(temp_dado) <- var_resposta
+
+      grafico <- ggplot(temp_dado, aes(sample = .data[[var_resposta]])) +
+        geom_qq() +
+        geom_qq_line() +
+        labs(title = paste('QQ-plot resíduos de', var_resposta),
+             x = 'Quantil teórico',
+             y = 'Quantil amostral'
+        ) +
+        theme_classic()
+
+      qq_list <- append(qq_list, list(grafico))
+    }
+    names(qq_list) <- var_respostas
   }
 
   # Gráfico de Resíduos vs Ordem de coleta (rvo)
-  if('rvo' %in% tipo) {
-    temp_dado <- as.data.frame(modelo$residuos)
-    grafico <- ggplot(data = temp_dado, aes(y = V1, x = 1:length(modelo$residuos))) +
-      geom_point() +
-      labs(
-        title = 'Resíduos x Ordem dos dados',
-        y = 'Resíduos',
-        x = 'Ordem dos dados'
-      )
-    rvo_list <- append(rvo_list, list(grafico_rvo))
-  }
+
+  # if('rvo' %in% tipo) {
+  #   temp_dado <- as.data.frame(modelo$residuos)
+  #   grafico <- ggplot(data = temp_dado, aes(y = V1, x = 1:length(modelo$residuos))) +
+  #     geom_point() +
+  #     labs(
+  #       title = 'Resíduos x Ordem dos dados',
+  #       y = 'Resíduos',
+  #       x = 'Ordem dos dados'
+  #     )
+  # }
+
+
+
   # Qualquer outro tipo
   # else {
   #   stop("Erro: tipo de gráfico inválido. Use 'pvo' para preditos vs observados ou 'rvo' para resíduos vs ordem de coleta.")
@@ -72,12 +108,8 @@ grafico <- function(modelo, tipo) {
 
   result <- list('pvo' = pvo_list,
                  'rvp' = rvp_list,
-                 'rvo' = rvo_list)
+                 'qqplot' = qq_list)
   return(result)
 }
 
-#Coloquei as funções que estavam em regressão.linear.R em arquivos separados, e comecei a fazer a documentação da função reg_linear.
-
-summary(modelolara)
-grafico(modelolara, tipo = "pvo")
-
+grafico(reg_linear(iris[, c(1, 3)], iris[, c(2, 4)]), tipo = c('pvo', 'rvp', 'qqplot'))
