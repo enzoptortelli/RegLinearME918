f8758c8f742dcb33753d9d346dcf72efd6e2c1e7
LuizNunes2020 <l255403@dac.unicamp.br>
2024-10-02 17:08:30 -0300

    ajustando

--- R/RegLinearME918.R
+++ R/RegLinearME918.R
@@ -1,13 +1,19 @@
-#' RegLinearME918: Um Pacote para Regressão Linear Simples
+#' RegLinearME918: Um Pacote para Regressão Linear Simples e Múltipla
 #'
-#' O pacote RegLinearME918 oferece ferramentas para realizar regressão linear simples, fazer predições, e gerar gráficos com base nos modelos ajustados.
+#' O pacote RegLinearME918 oferece ferramentas para ajustar modelos de regressão linear simples e múltipla, fazer predições com base nos modelos ajustados e gerar gráficos que ajudam a visualizar e analisar os resultados.
 #'
 #' @section Funções principais:
 #' \itemize{
-#'   \item \code{\link{reg_linear}}: Ajusta um modelo de regressão linear.
-#'   \item \code{\link{predicao}}: Faz predições com base no modelo ajustado.
-#'   \item \code{\link{grafico}}: Gera um gráfico comparando os valores observados e preditos.
+#'   \item \code{\link{reg_linear}}: Ajusta um modelo de regressão linear com base em variáveis preditoras e respostas.
+#'   \item \code{\link{predicao}}: Realiza predições com base no modelo de regressão ajustado e novos dados preditores.
+#'   \item \code{\link{grafico}}: Gera gráficos para visualizar comparações entre valores observados e preditos, resíduos e QQ-plots dos resíduos.
 #' }
 #'
-#' @keywords internal
+#' @section Conjunto de dados:
+#' O pacote inclui um conjunto de dados simulado, \code{\link{rl_dataset}}, que pode ser utilizado para testar as funções do pacote.
+#'
+#' @docType package
+#' @name RegLinearME918
+#' @keywords regressão linear, predição, gráficos, modelos estatísticos, dados simulados
+#' @seealso \code{\link{reg_linear}}, \code{\link{predicao}}, \code{\link{grafico}}, \code{\link{rl_dataset}}
 "_PACKAGE"
