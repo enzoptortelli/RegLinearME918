2f9827192f2d619a80b8e416915168fe5079c5ce
enzo <enzoptortelli@gmail.com>
2024-10-05 09:40:53 -0300

    Alterei a descrição das colunas dos dados de exemplo para ficarem mais ilustrativas.

--- R/data.R
+++ R/data.R
@@ -1,14 +1,15 @@
-#' Conjunto de dados simulados
+#' @title Desempenho de computadores
 #'
+#' @description
 #' Este é um conjunto de dados de exemplo incluído no pacote, gerado artificialmente para fins ilustrativos.
 #'
-#' @format Um data frame simulando tempos de inicialização e preços de computadores com 1000 observações, 3 variáveis preditoras e 2 variáveis resposta.
+#' @format Objeto \code{data.frame} com cinco colunas:
 #' \describe{
-#'   \item{price ($)}{Valores gerados da seguinte forma: \code{price = 100 * gpu_benchmark + 50 * cpu_benchmark + 50 * ram_qty + 50 + rnorm(1000,0,1)}}
-#'   \item{boot_speed (s)}{Valores gerados da seguinte forma: \code{boot_speed = 40 - gpu_benchmark * 0.4 - cpu_benchmark * 0.8- ram_qty * 0.5 + rnorm(1000, 0, 1)}}
-#'   \item{cpu_benchmark}{Valores gerados a partir de uma distribuição normal, com média = 18 e desvio-padrão = 4}
-#'   \item{gpu_benchmak}{Valores gerados a partir de uma distribuição normal, com média = 18 e desvio-padrão = 4}
-#'   \item{ram_qty}{Valores gerados a partir de uma distribuição uniforme discreta, com possíveis valores = (4,6,8,12,16)}
+#'  \item{price}{Preço do computador (em dólares americanos);}
+#'  \item{boot_speed}{Tempo para que o computador inicialize o sistema operacional (em segundos);}
+#'  \item{gpu_benchmark}{Pontução do computador no benchmark PavarimMark3D™;}
+#'  \item{cpu_benchmark}{Pontuação do computador no benchmark PavarimMark™ }
+#'  \item{ram_qty}{Quantidade memória RAM no computador (em GB).}
 #' }
 #' @source Gerado artificialmente para fins de exemplo no pacote.
 "rl_dataset"
