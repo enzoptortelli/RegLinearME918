2562c19c158570a82d464ef2f8475421abdcc3f4
laradru <laraherrera0@gmail.com>
2024-10-06 13:13:18 -0300

    Adicionando o teste do Eric para predição. Falta verificar.

--- tests/testthat/test-predicao.R
+++ tests/testthat/test-predicao.R
@@ -16,3 +16,15 @@ test_that("Teste 11 reg_linear()", {
   valores <- matrix(c(1, 5, 2, 6, 3, 7, 4, 9, 8), nrow = 3, byrow = TRUE)
   expect_length(predicao(modelo, valores)$preditos, nrow(valores))
 })
+
+
+# Teste 3:
+test_that("Teste predicao()", {
+  set.seed(43)
+  df <- data.frame(x1 = rnorm(100), x2 = rnorm(100))
+  df$y <- df$x1 + df$x2*2 + rnorm(100)
+  modelo <- reg_linear(df[, c("x1", "x2")], df$y)
+  valores <- matrix (c(7,6), nrow = 1, byrow = TRUE)
+
+  expect_equal(format(ceiling(predicao(modelo, valores)$predicao_1[[2]][[1]]*100)/100, nsmall = 2), 19.38)
+})
